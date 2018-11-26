305453d446600c4a47136ec733be20240f015ba4
单小飞 <shanpengfei@duolaimei.net>
2018-11-26 19:26:56 +0800

    fix 修改依赖库 添加AFN依赖

---
+++ test01/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by spf on 2018/11/26.
+//  Copyright © 2018年 spf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test01/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by spf on 2018/11/26.
+//  Copyright © 2018年 spf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
